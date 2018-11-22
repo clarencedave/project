7b6ce2d59b90d84d6d47688166491b7038a99acc
Cla2 <clarencedave990@gmail.com>
2018-11-22 10:33:35 +0800

    Your Message

---
+++ src/components/header.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import logo from '../images/seattle-angel-logo.png'
+const Header = ({ siteTitle }) => (
+  <div
+    style={{
+      background: '#000080',
+      marginBottom: '1.45rem',
+    }}
+  >
+  
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+      
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+            
+          }}
+        >
+          {siteTitle}
+        </Link>
+        
+      </h1>
+    </div>
+
+<div class="header">
+  <a   href="/" >
+  <img src= {logo} alt="Angel"/>
+  </a>
+  
+
+  <div class="header-right">
+    <a class="active" href="#home">Home</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
+  
+  <div class="profit">
+  <p>A non-profit corporation. Sparking the flame of angel investing in the Northwest</p>
+  </div>
+</div>
+
+
+
+
+  </div>
+  
+)
+
+export default Header
